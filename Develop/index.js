b78ca34b6f131aed780826dfa5c1146d34d74474
Rick Johnson <carl4917@umn.edu>
2021-12-17 00:29:40 -0600

    finishing inital questions

--- Develop/index.js
+++ Develop/index.js
@@ -10,7 +10,7 @@ const questions = () => {
     return inquirer.prompt([
         {
           type: 'input'
-          name: 'name'
+          name: 'title'
           message: 'What is the title of your project? (required)',
           validate: projectTitle => {
             if (projectTitle) {
@@ -19,9 +19,71 @@ const questions = () => {
               console.log('Please enter a title!');
               return false;
             }
-
+          }  
         }
-    ])
+// Second question asks for project description and also does not proceed unless one is entered.
+        {
+            type: 'input'
+            name: 'description'
+            message: 'Enter a Project Description (required)',
+            validate: projectDescription => {
+              if (projectDescription) {
+                return true;
+              } else {
+                console.log('Please enter a description!');
+                return false;
+              }
+            }  
+        }
+// Third question asks for project install instructions if there are any.
+        {
+            type: 'input'
+            name: 'installInstructions'
+            message: 'Enter the related installation instructions if appllicable', 
+        }
+// Fourth question asks for project user information and does not proceed unless it is provided.
+        {
+            type: 'input'
+            name: 'usageInfo'
+            message: 'Enter the Projects Usage Information (required)',
+            validate: usage => {
+              if (usage) {
+                return true;
+              } else {
+                console.log('Please enter the User Information!');
+                return false;
+              }
+            }  
+        }
+// Fifth question asks for project contribution guidance and does not proceed unless it is provided.
+        {
+            type: 'input'
+            name: 'contGuidelines'
+            message: 'Enter the Projects Contribution Guidelines (required)',
+            validate: guidelines => {
+              if (guidelines) {
+                return true;
+              } else {
+                console.log('Please enter Contribution Guidelines!');
+                return false;
+              }
+            }  
+        }
+// Sixth question asks for project test instructions and does not proceed unless it is provided.
+
+        {
+            type: 'input'
+            name: 'testInstructions'
+            message: 'Enter some project test instructions  (required)',
+            validate: Instructions => {
+              if (Instructions) {
+                return true;
+              } else {
+                console.log('Please enter a description!');
+                return false;
+              }
+            }  
+        }
 
 };
 
